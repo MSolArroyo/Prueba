839bdeba243be71f42435ce9b735491051c33cf6
MSolArroyo <71154953+MSolArroyo@users.noreply.github.com>
2022-03-23 21:05:43 -0300

    Subir Archivos

---
+++ script.js
@@ -0,0 +1,11 @@
+var miPerro = {
+	"nombre": "Ben",
+	"raza": "Labrado",
+	"color": "Negro"
+	"edad": 7
+};
+
+var nombrePerro = miPerro.nombre;
+var razaPerro = miPerro.raza;
+var colorPerro = miPerro.color;
+var edadPerro = miPerro.edad;
